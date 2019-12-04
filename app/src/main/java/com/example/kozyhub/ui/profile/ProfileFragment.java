d80b2de572292b5254c6b595cf2e6b2b21dcbbac
Viriya Hendarta <viriya.hendarta@gmail.com>
2019-12-04 22:02:48 +0700

    bnyk

--- app/src/main/java/com/example/kozyhub/ui/profile/ProfileFragment.java
+++ app/src/main/java/com/example/kozyhub/ui/profile/ProfileFragment.java
@@ -5,14 +5,10 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
-import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
-import androidx.lifecycle.Observer;
-import androidx.lifecycle.ViewModelProviders;
 import androidx.navigation.Navigation;
 
 import com.example.kozyhub.R;
@@ -20,24 +16,45 @@ import com.example.kozyhub.model.User;
 import com.example.kozyhub.util.session.SessionManager;
 
 public class ProfileFragment extends Fragment {
-
-    private ProfileViewModel profileViewModel;
-
-    public View onCreateView(@NonNull LayoutInflater inflater,
-            ViewGroup container, Bundle savedInstanceState) {
-        profileViewModel =
-                ViewModelProviders.of(this).get(ProfileViewModel.class);
-        View root = inflater.inflate(R.layout.fragment_profile, container, false);
+    private View getGuestLayout(@NonNull LayoutInflater inflater, ViewGroup container) {
+        View root = inflater.inflate(R.layout.fragment_login, container, false);
 
         final Button btnLogin = root.findViewById(R.id.btn_login);
         btnLogin.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                SessionManager.setUser(new User("wkwk", "wkwk@wkwk.com", "081234567",100000));
+                SessionManager.setUser(new User("wkwk", "wkwk@wkwk.com", "081234567", 100000));
                 Navigation.findNavController(getActivity(), R.id.nav_host_fragment).navigate(R.id.action_navigation_profile_to_navigation_home);
                 Toast.makeText(getContext(), "Welcome, wkwk", Toast.LENGTH_LONG).show();
             }
         });
+
+        final View btnSignup = root.findViewById(R.id.textview_signup_link);
+        btnSignup.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Navigation.findNavController(getActivity(), R.id.nav_host_fragment).navigate(R.id.action_navigation_profile_to_registerFragment);
+            }
+        });
+
+        return root;
+    }
+
+    private View getMemberLayout(@NonNull LayoutInflater inflater, ViewGroup container) {
+        View root = inflater.inflate(R.layout.fragment_profile, container, false);
+
+        return root;
+    }
+
+    public View onCreateView(@NonNull LayoutInflater inflater,
+                             ViewGroup container, Bundle savedInstanceState) {
+        View root;
+
+        if (SessionManager.isIsLoggedIn()) {
+            root = getMemberLayout(inflater, container);
+        } else {
+            root = getGuestLayout(inflater, container);
+        }
 
         return root;
     }
