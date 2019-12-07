344a4f77c79966bc8b624d6b53da921bdb5635e6
Viriya Hendarta <viriya.hendarta@gmail.com>
2019-12-07 17:34:43 +0700

    register blm selesai

--- app/src/main/java/com/example/kozyhub/ui/register/RegisterFragment.java
+++ app/src/main/java/com/example/kozyhub/ui/register/RegisterFragment.java
@@ -13,10 +13,31 @@ import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.CheckBox;
 import android.widget.EditText;
+import android.widget.Toast;
 
 import com.example.kozyhub.R;
+import com.example.kozyhub.model.SuccessLogin;
+import com.example.kozyhub.model.User;
+import com.example.kozyhub.util.session.SessionManager;
+import com.squareup.moshi.JsonAdapter;
+import com.squareup.moshi.Moshi;
+
+import org.jetbrains.annotations.NotNull;
+
+import java.io.IOException;
+
+import okhttp3.Call;
+import okhttp3.Callback;
+import okhttp3.FormBody;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.RequestBody;
+import okhttp3.Response;
+import okhttp3.ResponseBody;
 
 public class RegisterFragment extends Fragment {
+    private boolean isDoingSomething = false;
+
     public RegisterFragment() {
     }
 
@@ -56,8 +77,56 @@ public class RegisterFragment extends Fragment {
                         String password,
                         String passwordConfirm) {
 
-        NavController navController = Navigation.findNavController(getActivity(), R.id.nav_host_fragment);
-        navController.navigate(R.id.action_registerFragment_to_registerSuccessFragment);
+//        NavController navController = Navigation.findNavController(getActivity(), R.id.nav_host_fragment);
+//        navController.navigate(R.id.action_registerFragment_to_registerSuccessFragment);
+
+        isDoingSomething = true;
+
+        final OkHttpClient client = new OkHttpClient();
+
+        RequestBody body = new FormBody.Builder()
+                .add("username", username)
+                .add("password", password).build();
+
+        Request request = new Request.Builder()
+                .url("https://kozyhub.com/api/categories/register.php").post(body).build();
+
+        client.newCall(request).enqueue(new Callback() {
+            @Override
+            public void onFailure(@NotNull Call call, @NotNull IOException e) {
+                isDoingSomething = false;
+            }
+
+            @Override
+            public void onResponse(@NotNull Call call, @NotNull Response response) throws IOException {
+                isDoingSomething = false;
+
+                ResponseBody responseBody = response.body();
+
+                if (!response.isSuccessful())
+                    throw new IOException("Unexpected code " + response);
+
+                final String jsonResponse = responseBody.string();
+                Moshi moshi = new Moshi.Builder().build();
+
+                JsonAdapter<SuccessLogin> jsonAdapter = moshi.adapter(SuccessLogin.class);
+                final SuccessLogin res = jsonAdapter.fromJson(jsonResponse);
+
+                getActivity().runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        if (res.info.equals("success")) {
+                            User u = res.session_info.get(0);
+                            SessionManager.getInstance().setUser(getActivity(), new User(u.PersonName, u.UserName, u.PersonEmail, u.PersonPhone1));
+                            Navigation.findNavController(getActivity(), R.id.nav_host_fragment).navigate(R.id.action_navigation_profile_to_navigation_home);
+                            Toast.makeText(getActivity(), "Welcome " + u.PersonName, Toast.LENGTH_SHORT).show();
+                        } else {
+                            Toast.makeText(getActivity(), res.result, Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
+            }
+        });
 
     }
 
